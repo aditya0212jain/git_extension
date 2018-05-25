5965c7766d8b369e22e74577d63a030248e29063
Aditya <aditya0212jain@gmail.com>
2018-05-25 16:01:32 +0200

    merged into ui

--- git_local_testing/toggle.js
+++ git_local_testing/toggle.js
@@ -20,7 +20,25 @@ function toggleSide(currentFile){
 
 }
 
+function hideTest(btn){
+
+}
+
+
 function showTest(btn){
+  if(btn.parentElement.parentElement.parentElement.parentElement.hasAttribute("style")==true){
+    var div = btn.parentElement.parentElement.parentElement.parentElement;
+    div.removeAttribute("style");
+    var id = div.id;
+    console.log(id);
+    var id2 = id[id.length-1];
+    id2 = parseInt(id2) +1;
+    console.log(id2);
+    var div2 = document.getElementById("diff-"+id2);
+    div2.removeAttribute("style");
+
+  }
+  else{
   document.body.classList.add("full-width");
   console.log(btn.innerHTML);
   console.log(btn.value);
@@ -62,8 +80,6 @@ function showTest(btn){
   var p1 = document.getElementById("aditya");
   var p2 = document.getElementById("tab_c");
 
-  //p1.innerHTML = testIndex;
-
   var mainIndex;
   for(i=0;i<fileName.length;i++){
     if(i!=testIndex){
@@ -78,9 +94,6 @@ function showTest(btn){
   }
 
   //p2.innerHTML = mainIndex;
-  for(k=0;k<4;k++){
-    console.log(divContainer[k]);
-  }
 
   for(i=0;i<parseInt(tab_counter.innerHTML);i++){
     var div = document.getElementById("diff-"+i);
@@ -100,7 +113,12 @@ function showTest(btn){
     div.innerHTML = divContainer[i];
     }
   }
-
+  console.log(btn.innerHTML);
+  btn.innerHTML="Hide Test";
+  console.log(btn.innerHTML);
+  btn.setAttribute("onclick","hideTest(this)");
+  console.log(btn.onclick);
+  }
 
 }
 
@@ -110,7 +128,7 @@ function addButton(index,fileName){
   var fileAction = div0.getElementsByClassName("file-actions")[0];
   var btnGroup = fileAction.getElementsByClassName("BtnGroup")[0];
   var btn = document.createElement("BUTTON");
-  var t = document.createTextNode("Show Test");
+  var t = document.createTextNode("Test");
   btn.appendChild(t);
   btn.classList.add("btn");
   btn.classList.add("btn-sm");
@@ -123,22 +141,56 @@ function addButton(index,fileName){
 
 function placeBtn(){
   var d1 = document.getElementsByClassName("diffbar")[0];
-  var sortProd = document.createElement("BUTTON");
-  var textnode = document.createTextNode("Sort test first");
-  sortProd.appendChild(textnode);
-  sortProd.classList.add("btn");
-  sortProd.classList.add("btn-sm");
-  sortProd.classList.add("diffbar-item");
-  sortProd.setAttribute("onclick","sort(1)");
-  d1.insertBefore(sortProd, d1.childNodes[5]);
-  var sortProd = document.createElement("BUTTON");
-  var textnode = document.createTextNode("Sort production first");
-  sortProd.appendChild(textnode);
-  sortProd.classList.add("btn");
-  sortProd.classList.add("btn-sm");
-  sortProd.classList.add("diffbar-item");
-  sortProd.setAttribute("onclick","sort(0)");
-  d1.insertBefore(sortProd,d1.childNodes[5]);
+  // var sortProd = document.createElement("BUTTON");
+  // var textnode = document.createTextNode("Sort test first");
+  // sortProd.appendChild(textnode);
+  // sortProd.classList.add("btn");
+  // sortProd.classList.add("btn-sm");
+  // sortProd.classList.add("diffbar-item");
+  // sortProd.setAttribute("onclick","sort(1)");
+  // d1.insertBefore(sortProd, d1.childNodes[5]);
+  // var sortProd = document.createElement("BUTTON");
+  // var textnode = document.createTextNode("Sort production first");
+  // sortProd.appendChild(textnode);
+  // sortProd.classList.add("btn");
+  // sortProd.classList.add("btn-sm");
+  // sortProd.classList.add("diffbar-item");
+  // sortProd.setAttribute("onclick","sort(0)");
+  //d1.insertBefore(sortProd,d1.childNodes[5]);
+
+  var btn1 = document.createElement("BUTTON");
+  var btn2 = document.createElement("BUTTON");
+  var text1 = document.createTextNode("Production First");
+  btn1.appendChild(text1);
+  var text2 = document.createTextNode("Test First");
+  btn2.appendChild(text2);
+  btn1.setAttribute("class","btn btn-sm");
+  btn1.setAttribute("type","submit");
+  btn2.setAttribute("class","btn btn-sm");
+  btn1.setAttribute("onclick","sort(0);this.parentElement.parentElement.parentElement.removeAttribute('open');");
+  btn2.setAttribute("onclick","sort(1);this.parentElement.parentElement.parentElement.removeAttribute('open');");
+  var div3 = document.createElement("div");
+  div3.setAttribute("style","left:-83px");
+  div3.setAttribute("class","Popover-message text-left p-3 mx-auto Box box-shadow-large");
+  div3.appendChild(btn2);
+  div3.appendChild(btn1);
+  var div2 = document.createElement("div");
+  div2.setAttribute("class","Popover");
+  div2.appendChild(div3);
+  var sum = document.createElement("summary");
+  sum.setAttribute("class","btn-link muted-link select-menu-button js-menu-target");
+  var text3 = document.createTextNode("Sort");
+  var strong = document.createElement("strong");
+  strong.appendChild(text3);
+  sum.appendChild(strong);
+  var detail = document.createElement("details");
+  detail.setAttribute("class","details-reset");
+  detail.appendChild(div2);
+  detail.appendChild(sum);
+  var div1 = document.createElement("div");
+  div1.setAttribute("class","diffbar-item");
+  div1.appendChild(detail);
+  d1.insertBefore(div1,d1.childNodes[3]);
 }
 
 
