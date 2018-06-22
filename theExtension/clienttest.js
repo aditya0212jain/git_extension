063e666692d18c455e890064614e70177b4867d8
Aditya <aditya0212jain@gmail.com>
2018-06-22 17:38:59 +0200

    working fine on windows

--- theExtension/clienttest.js
+++ theExtension/clienttest.js
@@ -22,8 +22,10 @@ function addSpans(){
       //console.log(children[i].tagName);
       var tn = children[i].tagName;
       //te = te.replace(/\t/g,"    ");
-      var re = te.replace(/( )+|([a-zA-Z$_]+)|(((\()*(\))*(;)*)+)|(\t)+/g,"<span>$1$2$3$4<\/span>");
-      //children[i].textContent = re;
+      var re = te.replace(/( )+|([a-zA-Z$_]+)|(\()/g,"<span>$1$2$3<\/span>");
+      re = re.replace(/(\t)/g,"<span>$1</span>");
+      //re = re.replace(/((\()*(\))*(;)*)+/g,"<span>$1<\/span>");
+      //children[i].textContent = re;    (((\()*(\))*(;)*)+)
       if(children[i].tagName==undefined){
         var y = document.createElement("span");
         y.innerHTML = re;
@@ -36,7 +38,18 @@ function addSpans(){
       for(o=0;o<childOfChild.length;o++){
         childOfChild[o].addEventListener("mouseover", function(event){event.target.style.color = "orange";},false);
         childOfChild[o].addEventListener("mouseout", function(event){ event.target.style.color=""},false);
-        //childOfChild[o]
+        childOfChild[o].addEventListener("click", function(){
+          var linet = getLine(this.parentElement.parentElement);
+          console.log("the line is: "+ linet);
+          var chart = getCharacter(this);
+          console.log("the character no. is: " + chart);
+          var pReq = new XMLHttpRequest();
+          pReq.addEventListener("load", reqListener);
+          pReq.open("POST", "http://localhost:8080");
+          pReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          pReq.send(JSON.stringify({ line : linet,character: chart }));
+
+        },false)
       }
     }
 
@@ -75,7 +88,7 @@ function getLine(element){
   if(element.parentElement!=undefined || element.parentElement!=null){
     if(element.parentElement.tagName=="TR"){
       var td2 = element.parentElement.getElementsByClassName("js-line-number")[0];
-      console.log(td2.getAttribute("data-line-number"));
+      //console.log(td2.getAttribute("data-line-number"));
       return td2.getAttribute('data-line-number')-1;
     }
     else{
@@ -88,12 +101,12 @@ function getLine(element){
         //console.log(td.tagName);
         if(td.tagName=="TD"){
           //console.log(td);
-          console.log(td.getAttribute('data-line-number'));
+          //console.log(td.getAttribute('data-line-number'));
           return td.getAttribute('data-line-number')-1;
         }else if (td.tagName=="TR"){
           //console.log(td);
           var td1 = td.getElementsByClassName("js-line-number")[0];
-          console.log(td1.getAttribute('data-line-number'));
+          //console.log(td1.getAttribute('data-line-number'));
           return td1.getAttribute('data-line-number')-1;
         }
       }
@@ -107,8 +120,30 @@ function getChar(element){
   var children = element.parentElement.childNodes;
   var co=0;
   for(var i=0;i<index;i++){
-    co += children[i].textContent.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;').length;
+    co += children[i].textContent.replace(/\t/g, '    ').length;
   }
   console.log(co);
   return co;
+}
+
+function getCharacter(element){
+  var count1 = getCharacterTill(element);
+  var parent = element.parentElement;
+  var count2 = 0;
+  if(parent!=undefined || parent!= null){
+  count2 = getCharacterTill(parent);
+  }
+  return count1+count2;//+element.textContent.replace(/\t/g,"    ").length
+}
+function getCharacterTill(element){
+  var index = $(element).index();
+  var count=0;
+  var parent = element.parentElement;
+  if(parent!=undefined||parent!=null){
+    var children = parent.childNodes;
+    for(i=0;i<index;i++){
+      count += children[i].textContent.replace(/\t/g,"    ").length;
+    }
+  }
+  return count;
 }
