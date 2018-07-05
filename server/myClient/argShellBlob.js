9681cb8dbfda01a857bfe64a369e09f7f862ab12
ishepard <spadini.davide@gmail.com>
2018-07-05 16:40:51 +0200

    stop tracking argShell

--- server/myClient/argShellBlob.js
+++
@@ -1,1 +0,0 @@
-console.log("G:/Repos/working");console.log("G:/gitFiles/git_extension/server/myClient/serverRepos");console.log("repodriller");console.log("master"); 
--- server/myClient/argShellPull.js
+++
@@ -1,1 +0,0 @@
-console.log("G:/Repos/working");console.log("G:/Repos/server");console.log("fastjson");console.log("alibaba:master");console.log("jasonjoo2010:map"); 
