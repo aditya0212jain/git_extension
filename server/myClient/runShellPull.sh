902fe070f782b188c6ade7b0345bce112a71c7fc
Aditya <aditya0212jain@gmail.com>
2018-07-05 13:06:50 +0200

    better shell files for pull

--- server/myClient/runShellPull.sh
+++ server/myClient/runShellPull.sh
@@ -9,14 +9,42 @@ SP="_"
 rm -r -f $SERVERFOLDER/$REPO*
 #rm -r -f $SERVERFOLDER/$REPO$SP$BRANCH1
 #rm -r -f $SERVERFOLDER/$REPO$SP$BRANCH2
-cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER
-cd $SERVERFOLDER/$REPO
-git checkout $BRANCH2
-cd ..
-mv $REPO $REPO$SP$BRANCH2
-cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER
-cd $SERVERFOLDER/$REPO
-git checkout $BRANCH1
-cd ..
-mv $REPO $REPO$SP$BRANCH1
-echo pullDone
+if [ -d $SERVERFOLDER/$REPO$SP$BRANCH1 ]   # for file "if [-f /home/rama/file]"
+   then
+      echo "dir present"
+      cd $SERVERFOLDER/$REPO$SP$BRANCH1
+      git checkout $BRANCH1
+      echo "now check out"
+   else
+      echo "dir not present"
+      cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER/
+      cd $SERVERFOLDER/$REPO
+      git checkout $BRANCH1
+      cd ..
+      mv $REPO $REPO$SP$BRANCH1
+fi
+if [ -d $SERVERFOLDER/$REPO$SP$BRANCH2 ]   # for file "if [-f /home/rama/file]"
+   then
+      echo "dir present"
+      cd $SERVERFOLDER/$REPO$SP$BRANCH2
+      git checkout $BRANCH2
+      echo "now check out"
+   else
+      echo "dir not present"
+      cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER/
+      cd $SERVERFOLDER/$REPO
+      git checkout $BRANCH2
+      cd ..
+      mv $REPO $REPO$SP$BRANCH2
+fi
+#cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER
+#cd $SERVERFOLDER/$REPO
+#git checkout $BRANCH2
+#cd ..
+#mv $REPO $REPO$SP$BRANCH2
+#cp -r $WORKINGFOLDER/$REPO $SERVERFOLDER
+#cd $SERVERFOLDER/$REPO
+#git checkout $BRANCH1
+#cd ..
+#mv $REPO $REPO$SP$BRANCH1
+#echo pullDone
