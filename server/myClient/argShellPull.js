e03c8747aa68ae4d9fdd1fe86ada87366dc233c8
Aditya <aditya0212jain@gmail.com>
2018-07-05 16:59:56 +0200

    removed arg shell from gitignore

--- server/myClient/argShellPull.js
+++ server/myClient/argShellPull.js
@@ -1,1 +1,1 @@
-console.log("G:/Repos/working");console.log("G:/Repos/server");console.log("fastjson");console.log("alibaba:master");console.log("jasonjoo2010:map"); 
+console.log("G:/Repos/working");console.log("G:/gitFiles/git_extension/server/myClient/serverRepos");console.log("repodriller");console.log("master");console.log("num_commits"); 
