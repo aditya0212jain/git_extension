c888ffe7ddd2b96600b47f6dc3e7e8eae4949f33
Aditya <aditya0212jain@gmail.com>
2018-07-18 11:19:48 +0200

    the documentation for extension added

--- theExtension/popup.js
+++ theExtension/popup.js
@@ -1,7 +1,23 @@
+/**
+ * @file Manages the popup of the browserAction of the extension
+ * @author Aditya Jain
+ */
+
+ /** @module popup */
+
+
+/**
+*Called when one of the popup buttons is clicked to send the request to background script("gitCloneRequestFromPopup")
+*@function
+*@param {string} type https/ssh
+*/
 function popupCloneFunction(type){
   chrome.runtime.sendMessage({method:"gitCloneRequestFromPopup",type:type});
 }
 
+/**
+*Setting up the click listeners for the buttons in the popup
+*/
 document.addEventListener("DOMContentLoaded",function(event){
   document.getElementById('httpsClone').addEventListener("click",function(){popupCloneFunction('https')});
   document.getElementById('sshClone').addEventListener("click",function(){popupCloneFunction('ssh')});
